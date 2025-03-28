27b8aea877bd4db5e840ef0f2ef477b8b784d2be
theorium-0 <ridley.nelson@outlook.com>
2025-03-28 14:14:44 -0500

    Added Google Analytics Scripting

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 // Countdown Timer
 const countdown = () => {
-    const countDate = new Date('April 28, 2025 00:00:00').getTime();
-    const now = new Date().getTime();
-    const gap = countDate - now;
-  
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
-  
-    document.getElementById('days').innerText = textDay;
-    document.getElementById('hours').innerText = textHour;
-    document.getElementById('minutes').innerText = textMinute;
-    document.getElementById('seconds').innerText = textSecond;
+  const countDate = new Date('April 28, 2025 00:00:00').getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.getElementById('days').innerText = textDay;
+  document.getElementById('hours').innerText = textHour;
+  document.getElementById('minutes').innerText = textMinute;
+  document.getElementById('seconds').innerText = textSecond;
 };
-  
+
+// Update the clock
 setInterval(countdown, 1000);
-  
+
 // Email Subscription Form
-document.getElementById('subscribeForm').addEventListener('submit', function(event) {
+document.getElementById('subscribeForm').addEventListener('submit', function (event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
-  alert(`Thank you! We'll notify you at ${email} when we launch.`);
+  window.location.replace("mailto:someone@something.com");
+  // alert(`Thank you! We'll notify you at ${email} when we launch.`);
   document.getElementById('email').value = '';
 });
 
